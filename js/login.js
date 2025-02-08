3e9c168b047148659e842bb4f439b8155e203b3f
Mosiur Rahman <mosiurmahfuz357@gmail.com>
2025-02-08 18:46:40 +0600

    Update login.js

--- js/login.js
+++ js/login.js
@@ -10,7 +10,7 @@ document.getElementById('btn-login').addEventListener('click',function(event){
 
     if(phoneNumber === '5' && pinNumber === '1234'){
         console.log('You are logged in');
-        window.location.href = '/home.html'
+        window.location.href = '/payoo-mobile-bank/home.html'
     }
     else{
         alert('You are give wrong pin or number')
